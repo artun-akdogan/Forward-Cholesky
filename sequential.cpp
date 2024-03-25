98e046ea89c59e69ff6a60b5315b361fc9df8155
artun-akdogan <artun.akdogan@gmail.com>
2024-03-25 14:08:11 +0300

    Add timing and logs

--- sequential.cpp
+++ sequential.cpp
@@ -8,6 +8,9 @@
 #include <cmath>
 #include <algorithm>
 
+#include <chrono>
+using namespace std::chrono;
+
 typedef unsigned int uint;
 typedef unsigned long long ullong;
 
@@ -27,10 +30,30 @@ bool intersect(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2)
     return false;
 }
 */
+
+const int tab32[32] = {
+    0, 9, 1, 10, 13, 21, 2, 29,
+    11, 14, 16, 18, 22, 25, 3, 30,
+    8, 12, 20, 28, 15, 17, 24, 7,
+    19, 27, 23, 6, 26, 5, 4, 31};
+
+int log2_32(uint32_t value)
+{
+    value |= value >> 1;
+    value |= value >> 2;
+    value |= value >> 4;
+    value |= value >> 8;
+    value |= value >> 16;
+    return tab32[(uint32_t)(value * 0x07C4ACDD) >> 27];
+}
+
 int main()
 {
+
+    auto beg = high_resolution_clock::now();
+
     std::ifstream file("ct20stif.mtx");
-    int num_row, num_col, num_lines, tot_lines=0;
+    int num_row, num_col, num_lines, tot_lines = 0;
 
     // Ignore comments headers
     while (file.peek() == '%')
@@ -42,7 +65,7 @@ int main()
     std::vector<std::vector<sparse>> matrix(num_row);
     std::vector<std::vector<sparse>> result(num_row);
     std::vector<std::set<uint>> graph(num_row);
-    std::vector<std::set<uint>> rev_graph(num_row);
+    std::vector<std::vector<uint>> rev_graph(num_row);
 
     for (uint l = 0; l < num_lines; l++)
     {
@@ -54,17 +77,21 @@ int main()
 
     file.close();
 
-    std::cout << "File read" << std::endl;
+    auto end1 = high_resolution_clock::now();
 
+    std::cout << "File read in " << duration_cast<milliseconds>(end1 - beg).count() << " ms" << std::endl;
 
     for (uint i = 0; i < num_row; i++)
     {
         for (uint l = 0; l < matrix[i].size(); l++)
         {
-            if(matrix[i][l].column < i){
+            if (matrix[i][l].column < i)
+            {
                 graph[matrix[i][l].column].insert(i);
-                //rev_graph[matrix[i][l].column].insert(i);
-            }else if(matrix[i][l].column > i){
+                // rev_graph[matrix[i][l].column].insert(i);
+            }
+            else if (matrix[i][l].column > i)
+            {
                 std::cout << "wrong" << std::endl;
                 return 0;
             }
@@ -72,10 +99,10 @@ int main()
     }
     auto fit = graph[0].begin();
 
-    for (; fit != graph[0].end(); fit++)
-    {
-        std::cout << *fit << " ";
-    }
+    auto end2 = high_resolution_clock::now();
+
+    std::cout << "Base graph created in " << duration_cast<milliseconds>(end2 - end1).count() << " ms" << std::endl;
+
     std::cout << rev_graph[0].size() << " " << graph[0].size() << std::endl;
     std::cout << "Finding fills" << std::endl;
 
@@ -83,21 +110,29 @@ int main()
 
     for (uint i = 0; i < num_row; i++)
     {
-        std::cout <<inserted << " " << i << " " << num_row << " " << graph[i].size() << std::endl;
         auto fit = graph[i].begin();
 
-        for (; fit != graph[i].end(); fit++)
+        if (fit == graph[i].end())
         {
-            auto sit = fit;
-            for(sit++; sit != graph[i].end(); sit++){
-                if(*fit==i || *sit==i){
+            continue;
+        }
+
+        auto sit = fit;
+        for (sit++; sit != graph[i].end(); sit++)
+        {
+            if (*fit == i || *sit == i)
+            {
                 std::cout << "wrong" << std::endl;
                 return 0;
-                }
-                graph[*fit].insert(*sit);
             }
+            graph[*fit].insert(*sit);
         }
     }
+
+    auto end3 = high_resolution_clock::now();
+
+    std::cout << "Found fills in " << duration_cast<milliseconds>(end3 - end2).count() << " ms" << std::endl;
+
     std::cout << rev_graph[0].size() << " " << graph[0].size() << std::endl;
     std::cout << "Reversing Graph" << std::endl;
 
@@ -107,21 +142,25 @@ int main()
 
         for (; fit != graph[i].end(); fit++)
         {
-            rev_graph[*fit].insert(i);
+            rev_graph[*fit].push_back(i);
         }
     }
     std::cout << rev_graph[0].size() << " " << graph[0].size() << std::endl;
 
-    std::cout << "Graph created" << std::endl;
+    auto end4 = high_resolution_clock::now();
+
+    std::cout << "Reversed graph in " << duration_cast<milliseconds>(end4 - end3).count() << " ms" << std::endl;
+
+    std::cout << "Graph created in " << duration_cast<milliseconds>(end4 - end1).count() << " ms" << std::endl;
 
     for (uint i = 0; i < num_row; i++)
     {
+        result[i].reserve(rev_graph[i].size() + 20);
         for (auto fit = rev_graph[i].begin(); fit != rev_graph[i].end(); fit++)
         {
             double tot = 0, mat_val = 0, res_val = 0;
             for (uint a = 0, b = 0; a < result[i].size() && b < result[*fit].size() && result[i][a].column < *fit && result[*fit][b].column < *fit;)
             {
-
                 if (result[i][a].column < result[*fit][b].column)
                 {
                     a++;
@@ -137,6 +176,7 @@ int main()
                     b++;
                 }
             }
+
             auto mat_it = std::lower_bound(matrix[i].begin(), matrix[i].end(), (uint)*fit,
                                            [](const sparse &lhs, const uint rhs)
                                            { return lhs.column < rhs; });
@@ -157,6 +197,12 @@ int main()
                 tot_lines++;
             }
         }
+
+        if (!(i % 1000))
+        {
+            std::cout << i << std::endl;
+        }
+
         double tot = 0, mat_val = 0;
         for (uint a = 0; a < result[i].size(); a++)
         {
@@ -176,7 +222,9 @@ int main()
         }
     }
 
-    std::cout << "Operation completed" << std::endl;
+    auto end5 = high_resolution_clock::now();
+
+    std::cout << "Operation completed in " << duration_cast<milliseconds>(end5 - end4).count() << " ms" << std::endl;
 
     std::ofstream ofile("result.mtx");
     ofile << num_row << " " << num_col << " " << tot_lines << "\n";
@@ -191,7 +239,11 @@ int main()
 
     ofile.close();
 
-    std::cout << "File written" << std::endl;
+    auto end6 = high_resolution_clock::now();
+
+    std::cout << "File written in " << duration_cast<milliseconds>(end6 - end5).count() << " ms" << std::endl;
+
+    std::cout << "Program completed in " << duration_cast<milliseconds>(end6 - beg).count() << " ms" << std::endl;
 
     return 0;
 }
--- test.m
+++ test.m
@@ -1,4 +1,4 @@
-fid=fopen("bcsstk03.mtx");
+fid=fopen("ct20stif.mtx");
 e=textscan(fid,'%f %f %f','CommentStyle','%');
 out = cell2mat(e);
 mat = chol(sparse(out(2:end, 1), out(2:end, 2), out(2:end, 3), out(1, 1), out(1, 2), out(1, 3)), 'lower');
@@ -7,6 +7,7 @@ fid=fopen("result.mtx");
 e=textscan(fid,'%f %f %f','CommentStyle','%');
 out_ = cell2mat(e);
 res = sparse(out_(2:end, 1), out_(2:end, 2), out_(2:end, 3), out_(1, 1), out_(1, 2), out_(1, 3));
+
 
 testmat = mat-res;
 display(norm(testmat, Inf)/norm(mat, Inf));
